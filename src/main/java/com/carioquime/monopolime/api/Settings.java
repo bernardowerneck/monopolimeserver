2db66beb0995f554f0d3308af3eeccf8fc9e3b9b
gustavoplena <gustavoplenamente@gmail.com>
2019-10-11 11:03:10 -0300

    Add sockets

---
+++ src/main/java/com/carioquime/monopolime/api/Settings.java
@@ -0,0 +1,6 @@
+package com.carioquime.monopolime.api;
+
+public class Settings {
+    int HOST_PORT = Integer.parseInt(System.getenv("HOST_PORT"));
+    String HOST_NAME = System.getenv("HOST_NAME");
+}
